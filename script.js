ea20d23e87dc591fa4d3e2a2bb224417a9cb9acf
Samrat <samrattdeula@gmail.com>
2024-12-28 15:35:57 +0545

    adding files

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-let login= document.querySelector("#login");
+let user= document.querySelector("#user");
 let cross= document.querySelector("#cross");
+let cross2= document.querySelector("#cross2");
+let signup= document.querySelector("#signup");
+let login= document.querySelector("#login");
+
+
+
+signup.addEventListener("click",()=>{
+    document.querySelector(".form-container2").classList.add("active");
+    document.querySelector(".form-container").classList.remove("active");
+    user.disabled=true;
+});
 
 login.addEventListener("click",()=>{
+    document.querySelector(".form-container").classList.add("active");
+    document.querySelector(".form-container2").classList.remove("active");
+    user.disabled=false;
+
+});
+
+user.addEventListener("click",()=>{
     document.querySelector(".form-container").classList.add("active");
 });
 
 cross.addEventListener("click",()=>{
     document.querySelector(".form-container").classList.remove("active");
+})
+
+cross2.addEventListener("click",()=>{
+    document.querySelector(".form-container2").classList.remove("active");
+    user.disabled=false;
 });
