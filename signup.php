f50e7e36d8ae0ed2eebb1f77cfeb6742cd36a066
Samrat <samrattdeula@gmail.com>
2025-01-13 17:36:10 +0545

    changing name

---
+++ signup.php
@@ -0,0 +1,67 @@
+<?php
+$server="localhost";
+$username="root";
+$password="";
+$database="texttologin";
+
+
+$conn =mysqli_connect($server,$username,$password,$database);
+
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+  
+    $username =$_POST["name"];
+    $email=$_POST["email"];
+    $password =$_POST["password"];
+    $cpassword =$_POST["cpassword"];
+    // $exists=false;
+
+    //check wether this username exists
+    $existSql="SELECT * FROM `users` WHERE name ='$username'";
+    $result=mysqli_query($conn,$existSql);
+    $numExistRows =mysqli_num_rows($result);
+
+
+    if($numExistRows>0){
+      // $exists= true;
+      // $showError=" Username already exists " ;
+      echo " <script>
+      
+        alert('username already exists');
+    
+    </script> ";
+
+     
+    }
+    else{
+      // $exists= false;
+    
+
+    if(($password==$cpassword)){
+        $sql="INSERT INTO `users` ( `name`, `email`,`password`, `dt`) VALUES 
+        ('$username', '$email','$password', current_timestamp());";
+
+        $result =mysqli_query($conn,$sql);
+
+        if($result){
+           
+            echo " <script>
+      
+            alert('Your account is created');
+        
+        </script> ";
+        }
+      }
+        else{
+            
+            echo " <script>
+      
+            alert('Passwords do not match ');
+        
+        </script> ";
+        }
+    
+      }
+}
+
+
+?>
